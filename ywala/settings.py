ab996971db441e13e13465fc1ed5fdd870ae4f5a
wofstech <gabrielabuka3@gmail.com>
2018-04-18 19:20:57 -0600

    'added more files'

--- ywala/settings.py
+++ ywala/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9taxaadad!#a3cup@1$8ob
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shrouded-island-40221.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
